8751090b6cffcdac9c88155352605d119b10d19e
Basma-Elsaify <basmaelsaify@gmail.com>
2024-04-23 21:27:55 +0100

    Update webApp.py

--- webApp.py
+++ webApp.py
@@ -9,16 +9,16 @@ if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
   st.write(df)
 
-  # Add some matplotlib code !
-  fig, ax = plt.subplots()
-  df.hist(
-    bins=8,
-    column="Age",
-    grid=False,
-    figsize=(8, 8),
-    color="#86bf91",
-    zorder=2,
-    rwidth=0.9,
-    ax=ax,
-  )
-  st.write(fig)
+  # # Add some matplotlib code !
+  # fig, ax = plt.subplots()
+  # df.hist(
+  #   bins=8,
+  #   column="imageName",
+  #   grid=False,
+  #   figsize=(8, 8),
+  #   color="#86bf91",
+  #   zorder=2,
+  #   rwidth=0.9,
+  #   ax=ax,
+  # )
+  # st.write(fig)
